d732bb0c3fba8d028b4a64ed734b5c966d4c14b2
Damion <wdmhust@163.com>
2024-03-19 22:44:52 +0800

    update PixelPos_MouseFollow File

--- PixelPos_MouseFollow/widget.cpp
+++ PixelPos_MouseFollow/widget.cpp
@@ -15,10 +15,12 @@ Widget::Widget(QWidget *parent)
     QPixmap pix = QPixmap::fromImage(disImage);
 
     pix.scaled(ui->lbl_pic->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
-    ui->lbl_pic->setPixmap(pix);  // label 显示图像
+    // label 显示图像
+    ui->lbl_pic->setPixmap(pix);
+    /*激活控件鼠标跟随属性，激活后在点击鼠标后进入mouseMoveEvent函数*/
+    /*如果不点击鼠标时想要在控件上触发mouseMoveEvent函数，就需要同时激活控件和窗口*/
     ui->lbl_pic->setMouseTracking(true);
     setMouseTracking(true);
-
 }
 
 Widget::~Widget()
@@ -34,7 +36,7 @@ void Widget::mouseMoveEvent(QMouseEvent *event)
         QPoint PicPoint = QPoint(pt.x()-rect.x(), pt.y()- rect.y());
         QString str = QString("(x:%1,y:%2)").arg(PicPoint.x()).arg(PicPoint.y());
         ui->lbl_pos->setText(str);
-        if(img.channels() == 1){
+        if(img.channels() == 1){            //单通道图像
             int grayValue;
             switch (img.type())
             {
@@ -60,21 +62,16 @@ void Widget::mouseMoveEvent(QMouseEvent *event)
                 grayValue = static_cast<int>(img.at<double>(Point(PicPoint.x(), PicPoint.y())));
                 break;
             }
+            QString str = QString("Gray Value：%1").arg(grayValue);
+            ui->lbl_pix->setText(str);
         }
-        else
+        else                                //多通道图像
         {
             int value_B = static_cast<int>(img.at<Vec3b>(Point(PicPoint.x(), PicPoint.y()))[0]);
             int value_G = static_cast<int>(img.at<Vec3b>(Point(PicPoint.x(), PicPoint.y()))[1]);
             int value_R = static_cast<int>(img.at<Vec3b>(Point(PicPoint.x(), PicPoint.y()))[2]);
             QString str = QString("B：%1, G：%2, R：%3").arg(value_B).arg(value_G).arg(value_R);
             ui->lbl_pix->setText(str);
-
         }
-
-//        qDebug()<<"x:"<<pt.x()-rect.x();
-////        qDebug()<<"xxx:"<<ptx.x();
-
-//        qDebug()<<"y:"<<pt.y()- rect.y();
     }
-
 }
